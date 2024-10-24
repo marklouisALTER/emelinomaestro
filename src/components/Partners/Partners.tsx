31f26309498c2a80fb0167d0424abffab4236d72
Mark Louis Bernardo <marklouisbernando318@gmail.com>
2024-10-24 20:09:12 +0800

    feat: added animation and prod section

--- src/components/Partners/Partners.tsx
+++ src/components/Partners/Partners.tsx
@@ -1,23 +1,33 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 interface PartnerProp  {
     image: string
+    duration: number
 }
 
 const partners:PartnerProp[]= [
     {
-        image:"https://s3-alpha-sig.figma.com/img/414f/cec1/da158fffe6db3938bd10fb44368ea1e0?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LiMw0VcLhruRaKH0IHt6NMaTnO6dexo2sDQEvqGkp8fuTQgP0jLgyhtfvu0nFkB6nE3UxYY8hVpxDuKNnimhWGX3Io68tQJPkPfgFKOlKl5rFW~jiv2TzJGVaye~ALwGAhYqcMMlhCGq4rFjrarpUsQ5wg28fqE00P94ARmtGPUbzBpj0UDWVok2J2JimI1-Md0LuQu-exrPQhucRMyOV8XnV7h2Y4MX-wDNHVc3my4Ou33XRdZFr31lXr1GrE3iuk71NgfMCi7aMhdUKVgI~OdCVUqqyBLRVFNmPgh~fTaf6RjivRJKhaylIIp0kPbA0bTvA6IMILJR3NwjCMu7HQ__"
+        image:"https://s3-alpha-sig.figma.com/img/414f/cec1/da158fffe6db3938bd10fb44368ea1e0?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LiMw0VcLhruRaKH0IHt6NMaTnO6dexo2sDQEvqGkp8fuTQgP0jLgyhtfvu0nFkB6nE3UxYY8hVpxDuKNnimhWGX3Io68tQJPkPfgFKOlKl5rFW~jiv2TzJGVaye~ALwGAhYqcMMlhCGq4rFjrarpUsQ5wg28fqE00P94ARmtGPUbzBpj0UDWVok2J2JimI1-Md0LuQu-exrPQhucRMyOV8XnV7h2Y4MX-wDNHVc3my4Ou33XRdZFr31lXr1GrE3iuk71NgfMCi7aMhdUKVgI~OdCVUqqyBLRVFNmPgh~fTaf6RjivRJKhaylIIp0kPbA0bTvA6IMILJR3NwjCMu7HQ__",
+        duration: 400
     },
     {
-        image:"https://s3-alpha-sig.figma.com/img/414f/cec1/da158fffe6db3938bd10fb44368ea1e0?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LiMw0VcLhruRaKH0IHt6NMaTnO6dexo2sDQEvqGkp8fuTQgP0jLgyhtfvu0nFkB6nE3UxYY8hVpxDuKNnimhWGX3Io68tQJPkPfgFKOlKl5rFW~jiv2TzJGVaye~ALwGAhYqcMMlhCGq4rFjrarpUsQ5wg28fqE00P94ARmtGPUbzBpj0UDWVok2J2JimI1-Md0LuQu-exrPQhucRMyOV8XnV7h2Y4MX-wDNHVc3my4Ou33XRdZFr31lXr1GrE3iuk71NgfMCi7aMhdUKVgI~OdCVUqqyBLRVFNmPgh~fTaf6RjivRJKhaylIIp0kPbA0bTvA6IMILJR3NwjCMu7HQ__"
+        image:"https://s3-alpha-sig.figma.com/img/414f/cec1/da158fffe6db3938bd10fb44368ea1e0?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LiMw0VcLhruRaKH0IHt6NMaTnO6dexo2sDQEvqGkp8fuTQgP0jLgyhtfvu0nFkB6nE3UxYY8hVpxDuKNnimhWGX3Io68tQJPkPfgFKOlKl5rFW~jiv2TzJGVaye~ALwGAhYqcMMlhCGq4rFjrarpUsQ5wg28fqE00P94ARmtGPUbzBpj0UDWVok2J2JimI1-Md0LuQu-exrPQhucRMyOV8XnV7h2Y4MX-wDNHVc3my4Ou33XRdZFr31lXr1GrE3iuk71NgfMCi7aMhdUKVgI~OdCVUqqyBLRVFNmPgh~fTaf6RjivRJKhaylIIp0kPbA0bTvA6IMILJR3NwjCMu7HQ__",
+        duration: 600
     },
     {
-        image:"https://s3-alpha-sig.figma.com/img/414f/cec1/da158fffe6db3938bd10fb44368ea1e0?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LiMw0VcLhruRaKH0IHt6NMaTnO6dexo2sDQEvqGkp8fuTQgP0jLgyhtfvu0nFkB6nE3UxYY8hVpxDuKNnimhWGX3Io68tQJPkPfgFKOlKl5rFW~jiv2TzJGVaye~ALwGAhYqcMMlhCGq4rFjrarpUsQ5wg28fqE00P94ARmtGPUbzBpj0UDWVok2J2JimI1-Md0LuQu-exrPQhucRMyOV8XnV7h2Y4MX-wDNHVc3my4Ou33XRdZFr31lXr1GrE3iuk71NgfMCi7aMhdUKVgI~OdCVUqqyBLRVFNmPgh~fTaf6RjivRJKhaylIIp0kPbA0bTvA6IMILJR3NwjCMu7HQ__"
+        image:"https://s3-alpha-sig.figma.com/img/414f/cec1/da158fffe6db3938bd10fb44368ea1e0?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LiMw0VcLhruRaKH0IHt6NMaTnO6dexo2sDQEvqGkp8fuTQgP0jLgyhtfvu0nFkB6nE3UxYY8hVpxDuKNnimhWGX3Io68tQJPkPfgFKOlKl5rFW~jiv2TzJGVaye~ALwGAhYqcMMlhCGq4rFjrarpUsQ5wg28fqE00P94ARmtGPUbzBpj0UDWVok2J2JimI1-Md0LuQu-exrPQhucRMyOV8XnV7h2Y4MX-wDNHVc3my4Ou33XRdZFr31lXr1GrE3iuk71NgfMCi7aMhdUKVgI~OdCVUqqyBLRVFNmPgh~fTaf6RjivRJKhaylIIp0kPbA0bTvA6IMILJR3NwjCMu7HQ__",
+        duration: 800
     },
     {
-        image:"https://s3-alpha-sig.figma.com/img/414f/cec1/da158fffe6db3938bd10fb44368ea1e0?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LiMw0VcLhruRaKH0IHt6NMaTnO6dexo2sDQEvqGkp8fuTQgP0jLgyhtfvu0nFkB6nE3UxYY8hVpxDuKNnimhWGX3Io68tQJPkPfgFKOlKl5rFW~jiv2TzJGVaye~ALwGAhYqcMMlhCGq4rFjrarpUsQ5wg28fqE00P94ARmtGPUbzBpj0UDWVok2J2JimI1-Md0LuQu-exrPQhucRMyOV8XnV7h2Y4MX-wDNHVc3my4Ou33XRdZFr31lXr1GrE3iuk71NgfMCi7aMhdUKVgI~OdCVUqqyBLRVFNmPgh~fTaf6RjivRJKhaylIIp0kPbA0bTvA6IMILJR3NwjCMu7HQ__"
+        image:"https://s3-alpha-sig.figma.com/img/414f/cec1/da158fffe6db3938bd10fb44368ea1e0?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LiMw0VcLhruRaKH0IHt6NMaTnO6dexo2sDQEvqGkp8fuTQgP0jLgyhtfvu0nFkB6nE3UxYY8hVpxDuKNnimhWGX3Io68tQJPkPfgFKOlKl5rFW~jiv2TzJGVaye~ALwGAhYqcMMlhCGq4rFjrarpUsQ5wg28fqE00P94ARmtGPUbzBpj0UDWVok2J2JimI1-Md0LuQu-exrPQhucRMyOV8XnV7h2Y4MX-wDNHVc3my4Ou33XRdZFr31lXr1GrE3iuk71NgfMCi7aMhdUKVgI~OdCVUqqyBLRVFNmPgh~fTaf6RjivRJKhaylIIp0kPbA0bTvA6IMILJR3NwjCMu7HQ__",
+        duration: 1000
     },
     {
-        image:"https://s3-alpha-sig.figma.com/img/414f/cec1/da158fffe6db3938bd10fb44368ea1e0?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LiMw0VcLhruRaKH0IHt6NMaTnO6dexo2sDQEvqGkp8fuTQgP0jLgyhtfvu0nFkB6nE3UxYY8hVpxDuKNnimhWGX3Io68tQJPkPfgFKOlKl5rFW~jiv2TzJGVaye~ALwGAhYqcMMlhCGq4rFjrarpUsQ5wg28fqE00P94ARmtGPUbzBpj0UDWVok2J2JimI1-Md0LuQu-exrPQhucRMyOV8XnV7h2Y4MX-wDNHVc3my4Ou33XRdZFr31lXr1GrE3iuk71NgfMCi7aMhdUKVgI~OdCVUqqyBLRVFNmPgh~fTaf6RjivRJKhaylIIp0kPbA0bTvA6IMILJR3NwjCMu7HQ__"
+        image:"https://s3-alpha-sig.figma.com/img/414f/cec1/da158fffe6db3938bd10fb44368ea1e0?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LiMw0VcLhruRaKH0IHt6NMaTnO6dexo2sDQEvqGkp8fuTQgP0jLgyhtfvu0nFkB6nE3UxYY8hVpxDuKNnimhWGX3Io68tQJPkPfgFKOlKl5rFW~jiv2TzJGVaye~ALwGAhYqcMMlhCGq4rFjrarpUsQ5wg28fqE00P94ARmtGPUbzBpj0UDWVok2J2JimI1-Md0LuQu-exrPQhucRMyOV8XnV7h2Y4MX-wDNHVc3my4Ou33XRdZFr31lXr1GrE3iuk71NgfMCi7aMhdUKVgI~OdCVUqqyBLRVFNmPgh~fTaf6RjivRJKhaylIIp0kPbA0bTvA6IMILJR3NwjCMu7HQ__",
+        duration: 1200
     },
 ]
 
@@ -25,13 +35,29 @@ export const Partners: React.FC = () => {
 
     return (
         <div className="flex items-center justify-center flex-col mt-36 overflow-hidden">
-            <h1 className="text-lg md:text-3xl font-inter font-bold">OUR COMPANY PARTNER</h1>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 p-4">
+            <h1
+                data-aos="fade-up"
+                data-aos-duration="300"
+                className="text-lg md:text-3xl font-inter font-bold"
+            >
+                OUR COMPANY PARTNER
+            </h1>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4">
                 {partners.map((partner, index) => (
-                    <div key={index} className="w-[200px] bg-white rounded-md flex items-center justify-center">
-                        <img src={partner.image} alt="Partner" className="w-[150px] h-[100px] object-contain"/>
-                    </div>
-                ))}                                                
+                <div
+                    data-aos="fade-up"
+                    data-aos-duration={partner.duration}
+                    data-aos-anchor-placement="center-bottom"
+                    key={index}
+                    className="max-w-[200px] w-full bg-white rounded-md flex items-center justify-center"
+                >
+                    <img
+                    src={partner.image}
+                    alt="Partner"
+                    className="w-full h-auto object-contain p-4"
+                    />
+                </div>
+                ))}
             </div>
         </div>
     )
--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -2,35 +2,49 @@
 interface StatisticsProps {
     stats: string;
     name: string;
+    duration: number;
 }
 
 const statistics:StatisticsProps[] = [
     {
         stats: "10+",
-        name:"Years of Experience"
+        name:"Years of Experience",
+        duration: 500
     },
     {
         stats: "50+",
-        name:"Successful Seminars"
+        name:"Successful Seminars",
+        duration: 700
     },
     {
         stats: "1k",
-        name:"Happy Clients"
+        name:"Happy Clients",
+        duration: 900
     },
     {
         stats: "100+",
-        name:"Guestings"
+        name:"Guestings",
+        duration: 1100
     },
 ]
 
 export const Statistics: React.FC = () => {
     return (
-      <div className='relative w-full flex items-center justify-center gap-6 text-white font-karla font-bold 
+      <div className='relative w-full flex z-[11] items-center justify-center gap-6 text-white font-karla font-bold 
         rounded-md'>
-            <div className="absolute bg-primary hidden md:flex md:gap-3 lg:gap-5 p-5 rounded-md shadow-lg shadow-gray-400">
+            <div 
+                data-aos="fade-up"
+                data-aos-duration="300"
+                className="absolute bg-primary hidden md:flex md:gap-3 lg:gap-5 p-5 rounded-md shadow-lg shadow-gray-400">
                 {statistics.map((stats, index) => (
 
-                    <div key={index} className='w-full flex items-center justify-evenly md:px-1 lg:px-5 gap-2'>
+                    <div 
+                        key={index} 
+                        data-aos="fade-up"
+                        data-aos-duration={stats.duration}
+                        data-aos-anchor-placement="center-bottom"
+                        className='w-full flex items-center justify-evenly md:px-1 lg:px-5 gap-2'
+                    >
                         <span className='text-[30px] lg:text-[44px] xl:text-[60px]'> {stats.stats} </span>
                         <span className='lg:text-[13px] xl:text-[17px] font-normal w-[97px]'>{stats.name}</span>
                     </div>
